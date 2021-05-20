51a9d62b3448c47e6302b8bf4eced6f5728c45e7
sepehr javid <javid.sepehr77@gmail.com>
2021-05-20 16:58:56 +0430

    add new user distribution to peer recv thread class

--- controllers/peer_recv_thread.py
+++ controllers/peer_recv_thread.py
@@ -5,8 +5,12 @@ import parse
 
 from meta_data.database import MetaDatabase
 from meta_data.models.data_node import DataNode
+from meta_data.models.directory import Directory
+from meta_data.models.permission import Permission
+from meta_data.models.user import User
 from valid_messages import (CONFIRM_HANDSHAKE, STOP_FRIENDSHIP, RESPOND_TO_INTRODUCTION, ACCEPT, INTRODUCE_PEER,
-                            MESSAGE_SEPARATOR, SEND_DB, UPDATE_DATA_NODE, UNBLOCK_QUEUEING, START_CLIENT_SERVER)
+                            MESSAGE_SEPARATOR, SEND_DB, UPDATE_DATA_NODE, UNBLOCK_QUEUEING, START_CLIENT_SERVER,
+                            NEW_USER)
 from session.exceptions import PeerTimeOutException
 from session.sessions import SimpleSession, FileSession, EncryptedSession
 
@@ -29,15 +33,34 @@ class PeerRecvThread(Thread):
                 self.handle_message(message)
 
     def handle_message(self, message):
-        if message.split(MESSAGE_SEPARATOR)[0] == INTRODUCE_PEER.split(MESSAGE_SEPARATOR)[0]:
+        command = message.split(MESSAGE_SEPARATOR)[0]
+        if command == INTRODUCE_PEER.split(MESSAGE_SEPARATOR)[0]:
             self.add_peer(message)
         elif message == SEND_DB:
             self.receive_db()
-        elif message.split(MESSAGE_SEPARATOR)[0] == UPDATE_DATA_NODE.split(MESSAGE_SEPARATOR)[0]:
+        elif command == UPDATE_DATA_NODE.split(MESSAGE_SEPARATOR)[0]:
             self.update_data_node(message)
+        elif command == NEW_USER.split(MESSAGE_SEPARATOR)[0]:
+            self.create_account(message)
         elif message == STOP_FRIENDSHIP:
             self.session.close()
             self.continues = False
+
+    def create_account(self, message):
+        meta_data = dict(parse.parse(NEW_USER, message).named)
+        username = meta_data.get("username")
+        password = meta_data.get("password")
+
+        user = User.fetch_by_username(username=username, db=self.db)
+
+        if user is None:
+            user = User(db=self.db, username=username, password=password)
+            user.save()
+            main_directory = Directory(db=self.db, title=Directory.MAIN_DIR_NAME, parent_directory_id=None)
+            main_directory.save()
+            permission = Permission(directory_id=main_directory.id, user_id=user.id, perm=Permission.READ_WRITE)
+            permission.save()
+            self.controller.inform_next_node(message)
 
     def receive_db(self):
         file_session = FileSession()
@@ -89,7 +112,7 @@ class PeerRecvThread(Thread):
             self.session.transfer_data(STOP_FRIENDSHIP)
             self.session = new_session
             self.controller.inform_next_node(
-                UPDATE_DATA_NODE.encode(ip_address=data_node.ip_address, rack_number=data_node.rack_number,
+                UPDATE_DATA_NODE.format(ip_address=data_node.ip_address, rack_number=data_node.rack_number,
                                         available_byte_size=data_node.available_byte_size))
 
         print("Thread ", self.controller.peers)
